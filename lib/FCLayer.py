fixing gradient shape issues

lib/FCLayer.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Activations import ReLU, Linear
+from Activations import ReLU, Sigmoid, Linear
 class FCLayer:
     def __init__(self,shape,activation,init=1e-3):
         """
@@ -15,9 +15,11 @@ class FCLayer:
         self.weights = []
         self.weights.append(np.random.randn(shape[0],shape[1])*init)
         self.weights.append(np.random.randn(shape[1])*init)
-
+        print activation
         if activation == 'relu':
             self.activation = ReLU()
+        elif activation == 'sigmoid':
+            self.activation = Sigmoid()
         else:
             self.activation = Linear()
 
@@ -31,7 +33,7 @@ class FCLayer:
             -out, (numpy array),  with size (batch size, self.shape[1]),
             layer output
         """
-
+        self.x = x
         self.h = x.dot(self.weights[0])+self.weights[1]
         self.a = self.activation.forward(self.h)
         return self.a
@@ -47,8 +49,8 @@ class FCLayer:
         """
         dlda = self.activation.gradient(delta)
 
-        dldw = dlda.T.dot(self.a)
-        dldb = dlda
+        dldw = self.x.T.dot(dlda)
+        dldb = np.mean(dlda, axis=0, keepdims=True)
         dldx = dlda.dot(self.weights[0].T)
 
-        return (dlda,dldw,dldx)
+        return (dldb,dldw),dldx

lib/Loss.py
@@ -2,15 +2,19 @@ import numpy as np
 
 class MSE:
     def forward(self,ytrue,ypredicted):
+        self.loss = np.mean(np.square(ytrue-ypredicted))
+        return self.loss
 
     def gradient(self,ytrue,ypredicted):
-
-class BinaryCrossEntropy:
-    def forward(self, ytrue, ypredicted):
-
-    def gradient(self, ytrue, ypredicted):
-
-class CategoricalCrossEntropy:
-    def forward(self, ytrue, ypredicted):
-
-    def gradient(self, ytrue, ypredicted):
+        self.gradient = -2*(ytrue-ypredicted)
+        return self.gradient
+#
+# class BinaryCrossEntropy:
+#     def forward(self, ytrue, ypredicted):
+#
+#     def gradient(self, ytrue, ypredicted):
+#
+# class CategoricalCrossEntropy:
+#     def forward(self, ytrue, ypredicted):
+#
+#     def gradient(self, ytrue, ypredicted):

test.py
@@ -1,8 +1,23 @@
-from lib import FCLayer
+from lib import FCLayer,Net,Loss
 import numpy as np
 
 fc = FCLayer.FCLayer((10,20),'relu')
+fc2 = FCLayer.FCLayer((20,2),'sigmoid')
+fc3 = FCLayer.FCLayer((2,4),'linear')
 x = np.random.randn(100,10)
+y = np.ones((100,4))
 delta = np.random.randn(100,20)
 out = fc.forward(x)
 grad = fc.gradient(delta)
+
+net = Net.Net()
+net.addLayer(fc)
+net.addLayer(fc2)
+net.addLayer(fc3)
+yhat = net.forward(x)
+
+loss = Loss.MSE()
+mse = loss.forward(y,yhat)
+delta = loss.gradient(y,yhat)
+
+gradients,delta = net.gradient(delta)